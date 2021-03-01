37e2ea45eaeccdf332315b6caf3857e77893a51d
Fozimat <fozimata@gmail.com>
2021-03-01 23:14:30 +0700

    add grid mode

--- app/src/main/java/com/fozimat/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/fozimat/myrecyclerview/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -28,6 +29,12 @@ class MainActivity : AppCompatActivity() {
         rvHeroes.adapter = listHeroAdapter
     }
 
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
@@ -38,13 +45,13 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setMode(selectMode: Int) {
-        when(selectMode) {
+    private fun setMode(selectedMode: Int) {
+        when(selectedMode) {
             R.id.action_list -> {
-
+                showRecyclerList()
             }
             R.id.action_grid -> {
-
+                showRecyclerGrid()
             }
             R.id.action_cardview -> {
 
